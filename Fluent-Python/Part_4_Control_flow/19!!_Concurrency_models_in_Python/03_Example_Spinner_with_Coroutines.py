9af5a428801438b458e252838d4a51fdffbb809b
Julien Marty <julienmarty0819@gmail.com>
2026-03-21 10:35:12 +0100

    update 21/03/26

---
+++ Fluent-Python/Part_4_Control_flow/19!!_Concurrency_models_in_Python/03_Example_Spinner_with_Coroutines.py
@@ -0,0 +1,41 @@
+import asyncio
+import itertools
+import time
+"""
+It is the job of OS schedulers to allocate CPU time to drive threads and processes. In contrast, coroutines are driven 
+by an application-level event loop that manages a queue of pending coroutines, drives them one by one, monitors events 
+triggered by I/O operations initiated by coroutines, and passes control back to the corresponding coroutine when each 
+event happens. The event loop and the library coroutines and the user coroutines all execute in a single thread. 
+Therefore, any time spent in a coroutine slows down the event loop—and all other coroutines.
+"""
+
+async def spin(msg: str) -> None:
+    for char in itertools.cycle(r'\/-'):
+        status = f'\r{char} {msg}'
+        print(status, end='', flush=True)
+        try:
+            await asyncio.sleep(0.001) # spin wakes up every millisecond
+        except asyncio.CancelledError:
+            break
+
+    blanks = ' ' * len(status)
+    print(f'\r{blanks}\r', end='')
+
+""" Blocks the calling thread but release the GIL so the spinner thread can proceed """
+async def slow() -> int:
+    await asyncio.sleep(3)
+    return 42
+
+async def supervisor() -> int:
+    spinner_task = asyncio.create_task(spin("Thinking!"))
+    print(f'spinner object: {spinner_task}')
+    result = await slow()
+    spinner_task.cancel()
+    return result
+
+def main() -> None:
+    result = asyncio.run(supervisor())
+    print(f'Answer: {result}')
+
+if __name__ == '__main__':
+    main()
